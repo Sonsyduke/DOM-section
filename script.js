07e58fe3f86dd552ebbb13c52a3fbd8edd884c60
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-05 11:20:56 +0200

    Adding children elements

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-container";
 
 container.appendChild(content);
+
+const redParagraph = document.createElement("p");
+redParagraph.classList.add("redText");
+redParagraph.style.color = "red";
+redParagraph.textContent = "Hey I'm red!";
+container.appendChild(redParagraph);
+
+const heading3 = document.createElement("h3");
+heading3.style.color = "blue";
+heading3.textContent = "I'm a blue h3";
+container.appendChild(heading3);
+
+const mainDiv = document.createElement("div");
+const headingDiv = document.createElement("h1");
+const paragraphDiv = document.createElement("p");
+
+mainDiv.style.cssText = "border: 1px solid black; background-color: pink;";
+
+headingDiv.textContent = "I'm in a div";
+paragraphDiv.textContent = "ME TOO!";
+mainDiv.append(headingDiv, paragraphDiv);
+
+container.appendChild(mainDiv);
